6aaf1238884c975ff11e6890e5669c7e45d38659
Vlad <vlad210694@yahoo.com>
2019-05-28 11:22:56 +0200

    Added some flavour
    More rocks and walls, nothign special here

--- Assets/Scripts/Camera/MainCamera.cs
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -15,17 +15,51 @@ namespace Camera
         public Vector3 offset;
         private Vector3 startingPosition;
         public bool isLocked;
+        private float cameraX;
+        private float cameraY;
+        private Vector3 cameraBounds;
 
         public float dampTime = 0.15f;
         private Vector3 velocity = Vector3.zero;
 
-        public bool bounds;
-        public Vector3 minCamPos;
-        public Vector3 maxCamPos;
-
         void Start()
         {
             startingPosition = transform.position;
+            cameraX = OrthographicBounds(GetComponent<UnityEngine.Camera>()).x;
+            cameraY = OrthographicBounds(GetComponent<UnityEngine.Camera>()).y;
+            cameraBounds = OrthographicBounds(GetComponent<UnityEngine.Camera>());
+        }
+
+        void Update()
+        {
+            //toggle spacebar for locked/unlocked camera
+            if (Input.GetKeyDown("space"))
+            {
+                isLocked = !isLocked;
+            }
+            
+           
+//            RaycastHit2D hitleft = Physics2D.Raycast(transform.position,  Vector2.left, -cameraX);
+//            RaycastHit2D hitright = Physics2D.Raycast(transform.position, Vector2.right + cameraBounds, cameraX);
+//            RaycastHit2D hitdown = Physics2D.Raycast(transform.position, Vector2.down + cameraBounds, -cameraY);
+//            RaycastHit2D hitup = Physics2D.Raycast(transform.position, Vector2.up + cameraBounds, cameraY);
+//            
+//            if (hitleft.collider != null && hitright.collider.CompareTag("CameraBounds") )
+//            {
+//               Debug.Log(hitleft.collider + "left");
+//            }
+//            if (hitright.collider != null && hitright.collider.CompareTag("CameraBounds"))
+//            {
+//                Debug.Log(hitright.collider + "right");
+//            }
+//            if (hitdown.collider != null && hitdown.collider.CompareTag("CameraBounds"))
+//            {
+//                Debug.Log(hitdown.collider + "down");
+//            }
+//            if (hitup.collider != null && hitup.collider.CompareTag("CameraBounds"))
+//            {
+//                Debug.Log(hitup.collider + "up");
+//            }
         }
 
         void LateUpdate()
@@ -34,22 +68,26 @@ namespace Camera
             Vector3 delta = mainCamera.position - GetComponent<UnityEngine.Camera>()
                                 .ViewportToWorldPoint(new Vector3(0.5f, 0.5f,
                                     point.z)); //(new Vector3(0.5, 0.5, point.z));
-
-
-            if (Input.GetKeyDown("space") && !isLocked)
+            
+            if (isLocked)
             {
-                isLocked = !isLocked;
                 Vector3 destination =
-                    new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z - 10) + delta;
+                        new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z - 10) + delta;
                 transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
             }
-
-            if (Input.GetKeyDown("space") && isLocked)
+            else
             {
-                isLocked = false;
                 Vector3 destination = startingPosition + delta;
                 transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
             }
+        }
+        
+        public static Vector3 OrthographicBounds(UnityEngine.Camera camera)
+        {
+            float screenAspect = (float)Screen.width / (float)Screen.height;
+            float cameraHeight = camera.orthographicSize * 2;
+           
+            return new Vector3(cameraHeight * screenAspect, cameraHeight, 0);
         }
     }
 }
