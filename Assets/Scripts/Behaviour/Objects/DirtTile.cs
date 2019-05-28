75a978d3899ee6ed9fd1d90d272f7f3c7dbca764
Vlad <vlad210694@yahoo.com>
2019-05-28 09:31:09 +0200

    Cleaned up stuff
    Is clean now, don't worry

---
+++ Assets/Scripts/Behaviour/Objects/DirtTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Behaviour.Objects
+{
+    public class DirtTile : MonoBehaviour
+    {
+   
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
--- Assets/Scripts/Camera/MainCamera.cs
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -14,6 +14,7 @@ namespace Camera
         public Transform mainCamera;
         public Vector3 offset;
         private Vector3 startingPosition;
+        public bool isLocked;
 
         public float dampTime = 0.15f;
         private Vector3 velocity = Vector3.zero;
@@ -21,28 +22,34 @@ namespace Camera
         public bool bounds;
         public Vector3 minCamPos;
         public Vector3 maxCamPos;
-   
- 
-        // Update is called once per frame
-        void Update () 
-        { 
-            Vector3 point = GetComponent<UnityEngine.Camera>().WorldToViewportPoint(mainCamera.position);
-            Vector3 delta = mainCamera.position - GetComponent<UnityEngine.Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 
-        
-            if (Input.GetKey("space")) {
-                Vector3 destination = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z-10) + delta;
+        void Start()
+        {
+            startingPosition = transform.position;
+        }
+
+        void LateUpdate()
+        {
+            Vector3 point = GetComponent<UnityEngine.Camera>().WorldToViewportPoint(mainCamera.position);
+            Vector3 delta = mainCamera.position - GetComponent<UnityEngine.Camera>()
+                                .ViewportToWorldPoint(new Vector3(0.5f, 0.5f,
+                                    point.z)); //(new Vector3(0.5, 0.5, point.z));
+
+
+            if (Input.GetKeyDown("space") && !isLocked)
+            {
+                isLocked = !isLocked;
+                Vector3 destination =
+                    new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z - 10) + delta;
                 transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-            } else {
+            }
+
+            if (Input.GetKeyDown("space") && isLocked)
+            {
+                isLocked = false;
                 Vector3 destination = startingPosition + delta;
                 transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
             }
-     
-        }
-
-        private void Start()
-        {
-            startingPosition = transform.position;
         }
     }
 }
