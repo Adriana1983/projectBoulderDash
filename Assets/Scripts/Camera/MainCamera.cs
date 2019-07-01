646998c8b1c43ced2313be3da7ac6b5f00f50848
Jimmy Bierenbroodspot <165425@novacollege.nl>
2019-07-01 12:58:24 +0200

    Add better camera

--- Assets/Scripts/Camera/MainCamera.cs
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 using Vector2 = UnityEngine.Vector3;
+using Behaviour.Player;
 
 namespace MainCamera
 {
@@ -10,12 +11,16 @@ namespace MainCamera
         private GameObject mainCamera;
         private GameObject player;
         private GameObject playerSpawn;
+        private Movement movementScript;
 
         [SerializeField] private Vector3 minCameraPosition;
         [SerializeField] private Vector3 maxCameraPosition;
+        private Vector3 currentPos;
 
         private float mapWidth;
         private float mapHeight;
+
+        private bool foundPos;
 
         private List<Vector3> cameraBounds = new List<Vector3>();
 
@@ -24,38 +29,40 @@ namespace MainCamera
             mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
             playerSpawn = GameObject.FindGameObjectWithTag("Door");
 
-            mapWidth = (int)maxCameraPosition.x;
-            mapHeight = (int)maxCameraPosition.y;
+            mapWidth = maxCameraPosition.x;
+            mapHeight = maxCameraPosition.y;
 
-            Camera.main.orthographicSize = mapHeight / 4;
-            Camera.main.aspect = (mapWidth / 4) / (mapHeight / 4);
+            Camera.main.orthographicSize = 5.5f;
         }
         void Start()
         {
             InitializeCameraBounds();
-            mainCamera.transform.position = WhereIs(playerSpawn.transform.position);
-            Debug.Log(IsInCoordinate(new Vector3(1.0f, 1.0f, 0), cameraBounds[0], cameraBounds[1]));
+            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, WhereIs(playerSpawn.transform.position), 10 * Time.deltaTime);
+            currentPos = new Vector3(0, 0, -10);
+            foundPos = false;
         }
 
         void Update()
-            {
+        {
             if(PlayerHasSpawned())
             {
-                Debug.Log("Player is in " + WhereIs(player.transform.position));
-                mainCamera.transform.position = WhereIs(player.transform.position);
+                movementScript = player.GetComponent<Movement>();
+                mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, WhereIs(player.transform.position), 10 * Time.deltaTime);
             }
         }
 
         private void InitializeCameraBounds()
         {
-            for (int y = 1; y < 4; y++)
-            {
-                for (int x = 1; x < 4; x++)
-                {
-                    // subtracts 0.5f to prevent seeing half blocks on the bounds
-                    cameraBounds.Add(new Vector3((mapWidth / 4) * x -0.5f, (mapHeight / 4) * y - 0.5f, -10));
-                }
-            }
+            // TODO find way to automate this
+            cameraBounds.Add(new Vector3(9.5f, 16, -10));
+            cameraBounds.Add(new Vector3(9.5f, 10.5f, -10));
+            cameraBounds.Add(new Vector3(9.5f, 5, -10));
+            cameraBounds.Add(new Vector3(19.5f, 16, -10));
+            cameraBounds.Add(new Vector3(19.5f, 10.5f, -10));
+            cameraBounds.Add(new Vector3(19.5f, 5, -10));
+            cameraBounds.Add(new Vector3(29.5f, 16, -10));
+            cameraBounds.Add(new Vector3(29.5f, 10.5f, -10));
+            cameraBounds.Add(new Vector3(29.5f, 5, -10));
         }
 
         private bool PlayerHasSpawned()
@@ -70,34 +77,55 @@ namespace MainCamera
 
         private bool IsInCoordinate(Vector3 item, Vector3 isInMin, Vector3 isInMax)
         {
-            if(item.x >= isInMin.x && item.x <= isInMax.x && item.y >= isInMin.y && item.y <= isInMax.y)
+            if (item.x > isInMin.x && item.y > isInMin.y && item.x < isInMax.x && item.y < isInMax.y)
             {
                 return true;
             }
-            else
-            {
-                return false;
-            }
+            return false;
         }
 
         private Vector3 WhereIs(Vector3 waldo)
         {
-            Vector3 newPos = waldo;
-            foreach (Vector3 bound in cameraBounds)
+            int moveDirection = movementScript.animationDirection;
+            Vector3 waldoPos = new Vector3(9.5f, 16, -10);
+            foreach(Vector3 bound in cameraBounds)
             {
-                if(IsInCoordinate(
-                    waldo, new Vector3(bound.x - 10, bound.y - 5.5f, bound.z), new Vector3(bound.x, bound.y, bound.z)))
+                if (IsInCoordinate(
+                    waldo, new Vector3(bound.x - 6, bound.y - 3, -10), new Vector3(bound.x + 6, bound.y + 3, -10)))
                 {
-                    newPos = bound;
-                    Debug.Log("Waldo is in " + waldo);
+                    if (moveDirection == 1 || moveDirection == 3)
+                    {
+                        waldoPos = new Vector3(waldo.x, bound.y, -10);
+                        break;
+                    }
+                    else if (moveDirection == 2 || moveDirection == 4)
+                    {
+                        waldoPos = new Vector3(bound.x, waldo.y, -10);
+                        break;
+                    }
                 }
                 else
                 {
-                    Debug.Log("Waldo is not in " + bound);
-                    continue;
+                    waldoPos = new Vector3(waldo.x, waldo.y, -10);
                 }
             }
-            return new Vector3(newPos.x, newPos.y, -10);
+            if (waldo.x < minCameraPosition.x)
+            {
+                waldoPos.x = minCameraPosition.x;
+            }
+            if (waldo.x > maxCameraPosition.x)
+            {
+                waldoPos.x = maxCameraPosition.x;
+            }
+            if (waldo.y < minCameraPosition.y) 
+            {
+                waldoPos.y = minCameraPosition.y;
+            }
+            if (waldo.y > maxCameraPosition.y)
+            {
+                waldoPos.y = maxCameraPosition.y;
+            }
+            return waldoPos;
         }
     }
 }
