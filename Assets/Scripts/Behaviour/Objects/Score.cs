c887d8080315e51c4c55bdc7d50987f6fadef7b3
Vlad <vlad210694@yahoo.com>
2019-07-08 09:52:08 +0200

    dwd

--- Assets/Scripts/Behaviour/Objects/Score.cs
+++ Assets/Scripts/Behaviour/Objects/Score.cs
@@ -29,7 +29,7 @@ public class Score : MonoBehaviour
     #endregion
 
     //cave variables
-    public char CurrentCave = 'N';
+    public char CurrentCave = 'F';
 
     public float caveTime = 150;
     public float amoebaMagicTime = 75; //this is both amoebaSlowGrowthTime as magicWallMillingTime
